09e50fa0824793d0eb450631fd7bb8bb1ea46f21
brandonalston <bca3@rice.edu>
2022-05-21 18:13:16 -0500

    Dataset update, Data pull and preprocess

--- UTILS.py
+++ UTILS.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 import random
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 from gurobipy import *
 import networkx as nx
 import csv
@@ -13,32 +14,83 @@ def get_data(name, target):
     # Ensure all features are in [0,1] through encoding process
     global data
     try:
-        data = pd.read_csv('Datasets/' + name + '.csv', na_values='?')
+        cols_dict = {
+            'auto-mpg': ['target','cylinders','displacement','horsepower','weight','acceleration','model-year','origin','car_name'],
+            'balance-scale': ['target','left-weight','left-distance','right-weight','right-distance'],
+            'banknote_authentication': ['variance-of-wavelet','skewness-of-wavelet','curtosis-of-wavelet','entropy','target'],
+            'blood_transfusion': ['R','F','M','T','target'],
+            'breast-cancer': ['target','age','menopause','tumor-size','inv-nodes',
+                              'node-caps','deg-malig','breast','breast-quad','irradiat'],
+            'car': ['buying','maint','doors','persons','lug_boot','safety','target'],
+            'climate': ['Study','Run','vconst_corr','vconst_2','vconst_3','vconst_4','vconst_5','vconst_7','ah_corr',
+                        'ah_bolus','slm_corr','efficiency_factor','tidal_mix_max','vertical_decay_scale','convect_corr',
+                        'bckgrnd_vdc1','bckgrnd_vdc_ban','bckgrnd_vdc_eq','bckgrnd_vdc_psim','Prandtl','target'],
+            'flare1': ['class','largest-spot-size','spot-distribution','activity','evolution','previous-24hr-activity',
+                       'historically-complex','become-h-c','area','area=largest-spot','c-target','m-target','x-target'],
+            'flare2': ['class', 'largest-spot-size', 'spot-distribution', 'activity', 'evolution','previous-24hr-activity',
+                       'historically-complex', 'become-h-c', 'area', 'area=largest-spot', 'c-target','m-target','x-target'],
+            'glass': ['Id', 'RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe', 'target'],
+            'hayes-roth': ['name', 'hobby', 'age', 'educational-level', 'marital-status', 'target'],
+            'house-votes-84': ['target','handicapped-infants','water-project-cost-sharing',
+                               'adoption-of-the-budget-resolution', 'physician-fee-freeze', 'el-salvador-aid',
+                               'religious-groups-in-schools', 'anti-satellite-test-ban', 'aid-to-nicaraguan-contras',
+                               'mx-missile','immigration','synfuels-corporation-cutback','education-spending',
+                               'superfund-right-to-sue','crime','duty-free-exports','export-administration-act-south-africa'],
+            'image_segmentation': ['target','region-centroid-col','region-centroid-row','region-pixel-count',
+                                   'short-line-density-5','short-line-density-2','vedge-mean','vegde-sd','hedge-mean',
+                                   'hedge-sd','intensity-mean','rawred-mean','rawblue-mean','rawgreen-mean','exred-mean',
+                                   'exblue-mean','exgreen-mean','value-mean','saturatoin-mean','hue-mean'],
+            'ionosphere': list(range(1,35))+['target'],
+            'iris': ['sepal-length','sepal-width','petal-length','petal-width','target'],
+            'kr-vs-kp': ['bkblk','bknwy','bkon8','bkona','bkspr','bkxbq','bkxcr','bkxwp','blxwp','bxqsq','cntxt','dsopp',
+                         'dwipd','hdchk','katri','mulch','qxmsq','r2ar8','reskd','reskr','rimmx','rkxwp','rxmsq','simpl',
+                         'skach','skewr','skrxp','spcop','stlmt','thrsk','wkcti','wkna8','wknck','wkovl','wkpos','wtoeg',
+                         'target'],
+            'monk1': ['target','a1','a2','a3','a4','a5','a6'],
+            'monk2': ['target','a1','a2','a3','a4','a5','a6'],
+            'monk3': ['target','a1','a2','a3','a4','a5','a6'],
+            'parkinsons': ['name','MDVP:Fo(Hz)','MDVP:Fhi(Hz)','MDVP:Flo(Hz)','MDVP:Jitter(%)','MDVP:Jitter(Abs)','MDVP:RAP',
+                           'MDVP:PPQ','Jitter:DDP','MDVP:Shimmer','MDVP:Shimmer(dB)','Shimmer:APQ3','Shimmer:APQ5',
+                           'MDVP:APQ','Shimmer:DDA','NHR','HNR','target','RPDE','DFA','spread1','spread2','D2','PPE'],
+            'soybean-small': list(range(1,36))+['target'],
+            'tic-tac-toe': ['top-left-square','top-middle-square','top-right-square','middle-left-square','middle-middle-square',
+                            'middle-right-square','bottom-left-square','bottom-middle-square','bottom-right-square','target'],
+            'wine-red': ['fixed-acidity','volatile-acidity','citric-acid','residual-sugar','chlorides',
+                         'free-sulfur dioxide','total-sulfur-dioxide','density','pH','sulphates','alcohol','target'],
+            'wine-white': ['fixed-acidity','volatile-acidity','citric-acid','residual-sugar','chlorides',
+                           'free-sulfur dioxide','total-sulfur-dioxide','density','pH','sulphates','alcohol','target']
+        }
+
+        if name in cols_dict:
+            if name not in ['glass','hayes-roth','parkinsons']:
+                data = pd.read_csv('Datasets/'+name+'.data', names=cols_dict[name])
+            elif name == 'glass':
+                data = pd.read_csv('Datasets/'+name+'.data', names=cols_dict[name], index_col='Id')
+            elif name == 'hayes-roth' or 'parkinsons':
+                data = pd.read_csv('Datasets/'+name+'.data', names=cols_dict[name], index_col='name')
+        data.name = name
         data = preprocess(data, target)
+        return data
     except:
-        print("Dataset Not Found or Error in Encoding Process!")
-    return data
+        print("Dataset Not Found or Error in Encoding Process!\n")
+        return
 
 
 def preprocess(data, target):
-    new_data = pd.DataFrame(index=data.index, columns=data.columns)
-    numeric_col = [f for f in data.columns if target != f and np.issubdtype(data[f].dtype, np.number)]
-    cat_col = [f for f in data.columns if (f in data.select_dtypes(include='object').columns or
-                                           f in data.select_dtypes(include='category').columns)
-               and f != target]
-    for f in numeric_col:
-        if 0 <= data[f].min() and data[f].max() <= 1:
-            new_data[f] = data[f]
-        elif len(data[f].unique()) == 1:
-            new_data[f] = LabelEncoder().fit_transform(data[f])
-        else:
-            new_data[f] = (data[f] - data[f].min()) / (data[f].max() - data[f].min())
-    for f in cat_col:
-        new_data[f] = LabelEncoder().fit_transform(data[f])
-        new_data[f] = (new_data[f] - new_data[f].min()) / (new_data[f].max() - new_data[f].min())
-
+    global columnTransformer
+    numerical_datasets = ['iris','wine-red','wine-white','breast-cancer','banknote_authentication', 'blood_transfusion',
+                          'climate','glass','image_segmentation','ionosphere','parkinsons']
+    categorical_datasets = ['balance-scale','car','kr-vs-kp','house-votes-84','hayes-roth',
+                            'monk1','monk2','monk3','soybean_small','tic-tac-toe']
+    temp_data = data.drop(target, axis=1)
+    if data.name in categorical_datasets:
+        columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), temp_data.columns)],sparse_threshold=0)
+    elif data.name in numerical_datasets:
+        columnTransformer = ColumnTransformer([('normalizer', MinMaxScaler(), temp_data.columns)],sparse_threshold=0)
+    # else:
+    new_data = pd.DataFrame(columnTransformer.fit_transform(temp_data))
     new_data[target] = data[target]
-
+    print(new_data.head(3))
     return new_data
 
 
@@ -197,7 +249,7 @@ def model_summary(opt_model, tree, test_set, rand_state, results_file):
     with open(results_file, mode='a') as results:
         results_writer = csv.writer(results, delimiter=',', quotechar='"')
         results_writer.writerow(
-            [opt_model.dataname, tree.height, len(opt_model.datapoints),
+            [opt_model.data.name, tree.height, len(opt_model.datapoints),
              test_acc / len(test_set), train_acc / len(opt_model.datapoints), opt_model.model.Runtime,
              opt_model.model.MIPGap, opt_model.model.ObjVal, opt_model.model.ObjBound, opt_model.modeltype,
              opt_model.model._septime, opt_model.model._sepnum, opt_model.model._sepcuts, opt_model.model._sepavg,
