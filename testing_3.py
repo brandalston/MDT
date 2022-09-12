590b4b530c2942c324b18921350be3bea7f2530c
brandonalston <bca3@rice.edu>
2022-09-12 07:22:53 -0500

    benchmark testing

--- testing_3.py
+++ testing_3.py
@@ -11,6 +11,7 @@ gen = ((obj, rank) for obj in obs for rank in ranks)
 data_names = ['breast-cancer']
 for obj, rank in gen:
     hp_info = {'objective': obj, 'rank': rank}
+    print('HP TYPE:', hp_info)
     model_runs.main(
         ["-d", data_names, "-h", heights, "-m", models, "-t", time_limit, "-p", hp_info,
          "-r", rand_states, "-w", warm_start, "-e", model_extras, "-f", file, "-l", False])
