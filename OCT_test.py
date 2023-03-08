8d2db2e7ed9b4c73c6b99b09c5039e51f7d0706b
brandonalston <bca3@rice.edu>
2023-03-08 16:51:21 -0600

    MBDT, SOCT, OCT, GOSDTg, DL8.5, Quant-BnB testing

--- OCT_test.py
+++ OCT_test.py
@@ -9,7 +9,6 @@ test_set = ['wine_white', 'iris', 'banknote', 'glass', 'climate', 'image', 'bloo
             'fico_binary', 'soybean_small', 'car', 'monk1', 'balance_scale', 'breast-cancer', 'monk2']
 finished = ['car', 'balance', 'kvk', 'iris', 'climate', 'blood', 'ionosphere']
 
-test_num = ['glass', 'image']
 test_cat = ['fico_binary', 'soybean_small', 'monk1', 'breast-cancer', 'monk2']
 heights = [2, 3, 4, 5]
 time_limit = 600
@@ -20,5 +19,5 @@ log_file = False
 ############ OCT ###############
 models = ['OCT-Multivariate']
 OCT_run.main(
-   ["-d", test_num+test_cat, "-h", heights, "-t", time_limit, "-m", models,
+   ["-d", test_cat, "-h", heights, "-t", time_limit, "-m", models,
     "-r", rand_states, "-f", file, "-l", log_file])
--- SOCT_test.py
+++ SOCT_test.py
@@ -2,14 +2,14 @@ import MBDT_runs
 from Benchmarks import FB_OCT, DL8_5, OCT_run, SOCT_run, GOSDTg
 
 numerical = ['iris', 'banknote', 'blood', 'climate', 'wine-white', 'wine-red',
-             'glass', 'image_segmentation', 'ionosphere', 'parkinsons']
+             'glass', 'image', 'ionosphere', 'parkinsons']
 categorical = ['house_votes_84', 'hayes_roth', 'breast_cancer',
                'monk1', 'monk2', 'monk3', 'soybean_small', 'spect', 'tic_tac_toe', 'fico_binary']
-test_set = ['wine_white', 'iris', 'banknote', 'glass', 'climate', 'image_segmentation', 'blood', 'ionosphere',
+test_set = ['wine_white', 'iris', 'banknote', 'glass', 'climate', 'image', 'blood', 'ionosphere',
             'fico_binary', 'soybean_small', 'car', 'monk1', 'balance_scale', 'breast-cancer', 'monk2']
 finished = ['car', 'balance', 'kvk', 'iris', 'climate', 'blood', 'ionosphere']
 
-test_num = ['glass', 'image_segmentation']
+test_num = ['image']
 test_cat = ['fico_binary', 'soybean_small', 'monk1', 'breast-cancer', 'monk2']
 heights = [2, 3, 4, 5]
 time_limit = 600
