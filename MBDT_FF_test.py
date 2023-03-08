4952fae1685edbbfcc5f482989f311474bc681a7
brandonalston <bca3@rice.edu>
2023-03-08 16:58:00 -0600

    MBDT, SOCT, OCT, GOSDTg, DL8.5, Quant-BnB testing

--- MBDT_FF_test.py
+++ MBDT_FF_test.py
@@ -8,28 +8,19 @@ categorical = ['house_votes_84', 'hayes_roth', 'breast_cancer',
 test_set = ['wine_white', 'iris', 'banknote', 'glass', 'climate', 'image', 'blood', 'ionosphere',
             'fico_binary', 'soybean_small', 'car', 'monk1', 'balance_scale', 'breast-cancer', 'monk2']
 
-test_num = ['wine_white', 'iris', 'banknote', 'glass', 'climate', 'image', 'blood', 'ionosphere']
-test_cat = ['fico_binary', 'soybean_small', 'car', 'monk1', 'balance_scale', 'breast-cancer', 'monk2']
 heights = [2, 3, 4, 5]
 time_limit = 600
 rand_states = [138, 15, 89, 42, 0]
 file = 'paper_runs_2.csv'
 log_file = False
 
+test_cat = ['fico_binary', 'soybean_small', 'car', 'monk1', 'balance_scale', 'breast-cancer', 'monk2']
+test_num = ['climate', 'image', 'blood', 'ionosphere']
 ############ MBDT ###############
 models = ['CUT1-FF-ROOT', 'CUT2-FF-ROOT']
 b_type = ['SVM', 'ISING']  # CHOOSE ONE
 extras = None
 warm_start = {'use': False, 'values': None}
-
-test_num = ['banknote']
-heights = [5]
-MBDT_runs.main(
-   ["-d", test_num, "-h", heights, "-t", time_limit, "-m", models, "-b", b_type[0],
-    "-r", rand_states, "-f", file, "-e", extras, "-w", warm_start, "-l", log_file])
-
-heights = [2, 3, 4, 5]
-test_num = ['glass', 'climate', 'image', 'blood', 'ionosphere']
 MBDT_runs.main(
    ["-d", test_num+test_cat, "-h", heights, "-t", time_limit, "-m", models, "-b", b_type[0],
     "-r", rand_states, "-f", file, "-e", extras, "-w", warm_start, "-l", log_file])
